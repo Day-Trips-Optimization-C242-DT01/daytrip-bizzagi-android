c3394a930d2373f1bbfaa395ed8427bbef68c3e2
syahputerii <128578499+syahputerii@users.noreply.github.com>
2024-11-26 13:19:33 +0800

    update profile page

--- app/src/main/java/com/bizzagi/daytrip/ui/Profile/ProfileFragment.kt
+++ app/src/main/java/com/bizzagi/daytrip/ui/Profile/ProfileFragment.kt
@@ -28,30 +28,25 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Klik pada nama untuk pergi ke halaman EditProfileActivity
         binding.tvName.setOnClickListener {
-            // Pastikan EditProfileActivity sudah ada di AndroidManifest.xml
             val intent = Intent(requireContext(), EditProfileActivity::class.java)
             startActivity(intent)
         }
 
-        // Klik pada notifikasi untuk menunjukkan pesan Toast
         binding.tvNotification.setOnClickListener {
             Toast.makeText(requireContext(), "Notification clicked!", Toast.LENGTH_SHORT).show()
         }
 
-        // Klik pada bahasa untuk menunjukkan pesan Toast
         binding.tvLanguage.setOnClickListener {
             Toast.makeText(requireContext(), "Language clicked!", Toast.LENGTH_SHORT).show()
         }
 
-        // Klik pada Logout untuk mengonfirmasi logout
         binding.tvLogout.setOnClickListener {
             showLogoutConfirmationDialog()
         }
     }
 
-    // Fungsi untuk menampilkan konfirmasi logout
+    //Logout
     private fun showLogoutConfirmationDialog() {
         val builder = AlertDialog.Builder(requireContext())
         builder.setMessage("Are you sure you want to log out?")
@@ -61,15 +56,13 @@ class ProfileFragment : Fragment() {
                 logoutUser()
             }
             .setNegativeButton("No") { dialog, id ->
-                dialog.dismiss() // Tidak melakukan apa-apa jika batal
+                dialog.dismiss()
             }
         val alert = builder.create()
         alert.show()
     }
 
-    // Fungsi logout user (sementara dibiarkan sebagai komentar)
     private fun logoutUser() {
-        // Menghapus data pengguna dari SharedPreferences atau melakukan proses logout
         val sharedPreferences = requireActivity().getSharedPreferences("UserPrefs", AppCompatActivity.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
         editor.clear()
@@ -81,7 +74,6 @@ class ProfileFragment : Fragment() {
     override fun onResume() {
         super.onResume()
 
-        // Update nama pengguna yang ditampilkan
         val sharedPreferences = requireActivity().getSharedPreferences("UserPrefs", AppCompatActivity.MODE_PRIVATE)
         val updatedName = sharedPreferences.getString("USER_NAME", "Name") ?: "Name"
         binding.tvName.text = updatedName
