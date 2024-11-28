86d7a8e4668808841588244944b2dd6866f42a79
haris ardian <harisfirman387@gmail.com>
2024-11-28 08:32:24 +0700

    Token fix

--- app/src/main/java/com/bizzagi/daytrip/data/retrofit/ApiConfig.kt
+++ app/src/main/java/com/bizzagi/daytrip/data/retrofit/ApiConfig.kt
@@ -21,7 +21,7 @@ class ApiConfig {
                 .addInterceptor(loggingInterceptor)
                 .addInterceptor {chain ->
                     val request = chain.request().newBuilder()
-                        .addHeader("Authorization","Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjkyODg2OGRjNDRlYTZhOThjODhiMzkzZDM2NDQ1MTM2NWViYjMwZDgiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vYzI0Mi1kdDAxIiwiYXVkIjoiYzI0Mi1kdDAxIiwiYXV0aF90aW1lIjoxNzMyNzU2NjkxLCJ1c2VyX2lkIjoiVEJ0UkEySTF0UmdBV3FERlpGQXcwT3premhEMyIsInN1YiI6IlRCdFJBMkkxdFJnQVdxREZaRkF3ME96a3poRDMiLCJpYXQiOjE3MzI3NTY2OTEsImV4cCI6MTczMjc2MDI5MSwiZW1haWwiOiJ0ZXN0QHRlc3QuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbInRlc3RAdGVzdC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.Hjx5mPYjJXgT_LRj3J_UT0DBHTsOo_rmR0Z9hKFq9k5lYpea33Hzfah2ZUsraiecQcxnsvDu1jrhlYHArweUqgzcYCuN_BRvmoOKCGDJddyuGbyRERHrFUOo2yfrlYN6_jh8gZjKswR36sD75MFOcTUCMrTFLCipWgV4inVhS5u32BPFdDvUoa-aD2b7mcJ3A6X6DGvGYoJ-b9OQb9xeqipCcHP_t_KV981_zIYKxx2oUJnz9vzbdfRYRa2Z33VT10GY4W2pV8WO13zkGUs5rvkqgbEtzdmP1OWzKQoWmOrb-A6znpDZsXeVNFlDzSo6Isebth0oe6XQpgxIJm4X8g")
+                        .addHeader("Authorization","Bearer token")
                         .build()
                     chain.proceed(request)
                 }
