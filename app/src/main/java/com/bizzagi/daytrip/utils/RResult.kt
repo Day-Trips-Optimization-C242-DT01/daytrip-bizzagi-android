45610e9e6f845afd60341df279e6ee9c9fcefa8f
syahputerii <128578499+syahputerii@users.noreply.github.com>
2024-12-02 21:19:33 +0800

    delete unused file

--- app/src/main/java/com/bizzagi/daytrip/utils/RResult.kt
+++
@@ -1,7 +0,0 @@
-package com.bizzagi.daytrip.utils
-
-sealed class RResult<out R> private constructor() {
-    data class Success<out T>(val data : T) : RResult<T>()
-    data class Error(val error : String) : RResult<Nothing>()
-    data object Loading : RResult<Nothing>()
-}
